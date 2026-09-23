87385f5113abe4cb424f7793f8539c0420f6481c
Manu2015 <devries.webdesigner@gmail.com>
2026-09-23 11:40:04 +0200

    chore: remove next starter content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
-import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import "@/app/globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -20,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fr" className={inter.variable}>
       <body>{children}</body>
     </html>
   );
